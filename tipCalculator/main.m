6e288efd56f5085ec82eac9a1b054680d0499fba
Alberto Campos <xopmac@gmail.com>
2013-12-04 21:26:52 -0800

    Initial Commit

---
+++ tipCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipCalculator
+//
+//  Created by Alberto Campos on 12/4/13.
+//  Copyright (c) 2013 CampOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "tipCalcAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([tipCalcAppDelegate class]));
+    }
+}
---
+++ tipCalculator/tipCalcAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  tipCalcAppDelegate.h
+//  tipCalculator
+//
+//  Created by Alberto Campos on 12/4/13.
+//  Copyright (c) 2013 CampOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tipCalcAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
