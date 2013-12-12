f15985572f02666b94a6561af69176666f674d01
Alberto Campos <xopmac@gmail.com>
2013-12-11 23:09:20 -0800

    starting default settings

--- tipCalculator/SettingsViewController.m
+++ tipCalculator/SettingsViewController.m
@@ -9,6 +9,16 @@
 #import "SettingsViewController.h"
 
 @interface SettingsViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *guestsTextField;
+@property (weak, nonatomic) IBOutlet UITextField *minTipTextField;
+@property (weak, nonatomic) IBOutlet UITextField *avgTipTextField;
+@property (weak, nonatomic) IBOutlet UITextField *maxTipTextField;
+@property (weak, nonatomic) IBOutlet UILabel *resultsLabel;
+- (IBAction)resetButton:(id)sender;
+
+
+- (IBAction)onTap:(id)sender;
+- (void) onUpdateDefaults;
 
 @end
 
@@ -27,6 +37,9 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Reset" style:UIBarButtonItemStylePlain target:self action:@selector(onResetButton)];
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +47,36 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (void)onResetButton {
+    [self.navigationController pushViewController:[[SettingsViewController alloc] init] animated:YES];
+}
+- (IBAction)resetButton:(id)sender {
+}
+
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+    [self onUpdateDefaults];
+    
+}
+
+- (void) onUpdateDefaults
+{
+    int guestsAvg = [self.guestsTextField.text intValue];
+    float minTip = [self.minTipTextField.text floatValue]/100;
+    float avgTip = [self.avgTipTextField.text floatValue]/100;
+    float maxTip = [self.maxTipTextField.text floatValue]/100;
+    
+    NSString *s = @"";
+    
+    s =[NSString stringWithFormat:@"%d", guestsAvg ];
+    s = [s stringByAppendingString:@" guests. "];
+    s = [s stringByAppendingString: [NSString stringWithFormat:@"%0.2f", minTip]];
+    s = [s stringByAppendingString:@" percentage"];
+
+    
+    self.resultsLabel.text = s;
+    
+}
+
 
 @end
--- tipCalculator/tipViewController.m
+++ tipCalculator/tipViewController.m
@@ -25,10 +25,14 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    
+    
     if (self) {
         // Custom initialization
-        self.title = @"Tip Calculator";
+        self.title = @"Tip calculator";
     }
+    
     return self;
 }
 
@@ -36,8 +40,9 @@
 {
     [super viewDidLoad];
     [self updateValues];
-    
+    [self.billTextField becomeFirstResponder];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Settings" style:UIBarButtonItemStylePlain target:self action:@selector(onSettingsButton)];
+
     
 }
 
@@ -55,6 +60,8 @@
 - (void)updateValues {
     float billAmount = [self.billTextField.text floatValue];
     
+    
+    
     NSArray *tipValues = @[@(0.1), @(0.15), @(0.2)];
     float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
     
@@ -67,4 +74,21 @@
 - (void)onSettingsButton {
     [self.navigationController pushViewController:[[SettingsViewController alloc] init] animated:YES];
 }
+
+- (void)viewWillAppear:(BOOL)animated {
+    NSLog(@"view will appear");
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    NSLog(@"view did appear");
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    NSLog(@"view will disappear");
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    NSLog(@"view will disappear");
+}
+
 @end
